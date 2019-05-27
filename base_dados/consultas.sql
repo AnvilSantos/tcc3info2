181ae7fe97b818db6e3a01a67969ccc721bd2800
Willian-Jose <50213652+Willian-Jose@users.noreply.github.com>
2019-05-27 10:57:10 -0300

    consultas novas

--- base_dados/consultas.sql
+++ base_dados/consultas.sql
@@ -3,3 +3,9 @@ SELECT lugar, regente, desc_grupo, materia_grupo, dia_grupo from grupo;
 SELECT desc_atividade, materia_atividade, data_cadastro, data_entrega from atividade;
 
 SELECT nome, email from usuario;
+
+SELECT nome, email, desc_tipo from usuario, tip_user, tipo_usuario where usuario.cod_usuario = tipo_usuario.cod_usuario and tip_user.cod_tipo = tipo_usuario.cod_tipo;
+
+update tipo_usuario set cod_usuario where cod_usuario is null;
+
+insert into tipo_usuario(cod_usuario,cod_tipo) values (1,2),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1);
