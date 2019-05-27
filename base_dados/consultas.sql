dc9e062dc8996a67679d35e5794bb5fd4a74025d
Willian-Jose <50213652+Willian-Jose@users.noreply.github.com>
2019-05-27 09:53:16 -0300

    up consultas.sql

---
+++ base_dados/consultas.sql
@@ -0,0 +1,5 @@
+SELECT lugar, regente, desc_grupo, materia_grupo, dia_grupo from grupo;
+
+SELECT desc_atividade, materia_atividade, data_cadastro, data_entrega from atividade;
+
+SELECT nome, email from usuario;
