8903d94d00958741014de488a5da150ca1a66c64
Willian-Jose <50213652+Willian-Jose@users.noreply.github.com>
2019-06-05 09:07:21 -0300

    teste

---
+++ projeto/conteudo/cadastrar_usuario.php
@@ -0,0 +1,3 @@
+<h1>Cadastrar Usuário</h1>
+<form action="index.php?p=cadastrar" method="POST"></form>
+	<label for=""></label>
---
+++ projeto/conteudo/crud_usuario.php
@@ -0,0 +1,7 @@
+<?php
+	include 'cabecalho.php';
+?>
+
+<div style="width: 50%; margin: 0 auto; background-color: #FFF; border: 1px solid #e3e3e3; border-radius: 5px;" >  
+	
+</div>
