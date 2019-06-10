b5e3f7c8b35e08de1ca50981e1fdd72de85a016f
Willian-Jose <50213652+Willian-Jose@users.noreply.github.com>
2019-06-10 09:54:50 -0300

    consultas novas 2.0

--- base_dados/consultas.sql
+++ base_dados/consultas.sql
@@ -9,3 +9,15 @@ SELECT nome, email, desc_tipo from usuario, tip_user, tipo_usuario where usuario
 update tipo_usuario set cod_usuario where cod_usuario is null;
 
 insert into tipo_usuario(cod_usuario,cod_tipo) values (1,2),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1);
+
+SELECT nome, email, senha from usuario where email = $email and senha = $senha
+
+UPDATE usuario set nome = $nome where codigo = 1
+
+insert into usuario(nome,email,senha) values ($nome,$email,$senha)
+
+$sql = "SELECT senha as senha, cod_usuario as valor 
+        FROM usuario 
+        WHERE email = '$_SESSION[email]'";
+        $que = $mysqli->query($sql) or die($mysqli->error);
+        $dado = $que->fetch_assoc();	
